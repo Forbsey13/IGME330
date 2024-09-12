def1ac00d6b2c784854731fd70cfcfc618c2b2ac
Will Forbes <wforbes1313@gmail.com>
2024-09-12 18:11:49 -0400

    Working with technobabble Ultimate

--- HW/forbes-w-HW1/src/main.js
+++ HW/forbes-w-HW1/src/main.js
@@ -1,25 +1,19 @@
 "use strict";
 
-const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
-
-const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
-
-const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+let cl5Babble = false;
 
 function main() {
-
     generate();
-    document.querySelector("#myButton").onclick = generate;
-    // document.querySelector("#myButton").addEventListener("clcik",generate);
-
+    document.querySelector("#babble-1").onclick = generate(1);
+    document.querySelector("#babble-5").onclick = generate(5);
 }
-function generate() {
+
+const generate = (times) => {
     function randomElement(array) {
         return array = words1[Math.floor(Math.random() * array.length)];
     }
 
-    //return randomElement(words1) + " " +  randomElement(words2) + " " +  randomElement(words3);
     const str = `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}`;
     document.querySelector("#output").innerHTML = str;
-}
+};
 window.onload = main;
