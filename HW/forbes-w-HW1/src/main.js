9a3779c486a5268c1c1a80b7e401a87e77e210bc
Will Forbes <wforbes1313@gmail.com>
2024-09-15 13:23:42 -0400

    Working on ultimate babble

--- HW/forbes-w-HW1/src/main.js
+++ HW/forbes-w-HW1/src/main.js
@@ -1,19 +1,46 @@
-"use strict";
+import { randomElement } from "./utils.js";
 
-let cl5Babble = false;
-
-function main() {
-    generate();
-    document.querySelector("#babble-1").onclick = generate(1);
-    document.querySelector("#babble-5").onclick = generate(5);
-}
+let words1 = [];
+let words2 = [];
+let words3 = [];
 
 const generate = (times) => {
-    function randomElement(array) {
-        return array = words1[Math.floor(Math.random() * array.length)];
+    let str;
+    for (let i = 0; i < times; i++) {
+        str += `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}\n`;
     }
-
-    const str = `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}`;
     document.querySelector("#output").innerHTML = str;
 };
+
+const click = () => {
+    const babble1 = document.querySelector("#babble-1");
+    babble1.onclick = () => generate(1);
+
+    const babble5 = document.querySelector("#babble-5");
+    babble5.onclick = () => generate(5);
+
+    generate(1); //Load with 1
+};
+
+const loadBabble = (e) => {
+    const json = JSON.parse(e.target.responseText);
+    words1 = json.words1;
+    words2 = json.words2;
+    words3 = json.wrods3;
+    click(); //Run after filling arrays
+};
+
+
+const main = () => {
+    const url = "data/babble-data.json";
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = (e) => {
+        loadBabble(e);
+    }
+
+    xhr.open("GET", url);
+    xhr.send();
+};
+
 window.onload = main;
