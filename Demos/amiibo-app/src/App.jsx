a2878221b4a9b5d30b6be2969f013fb97160aad9
Will Forbes <wforbes1313@gmail.com>
2024-11-14 17:20:34 -0500

    React Demo

---
+++ Demos/amiibo-app/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import './App.css'
+
+// app "globals" and utils
+const baseurl = "https://www.amiiboapi.com/api/amiibo/?name=";
+
+const loadXHR = (url, callback) => {
+  // set up the connection
+  // when the data loads, invoke the callback function and pass it the `xhr` object
+};
+
+const searchAmiibo = (name, callback) => {
+  loadXHR( `${baseurl}${name}`, callback);
+};
+
+const App = () => {
+  return <>
+    <header>
+      <h1>Amiibo Finder</h1>
+    </header>
+    <hr />
+    <main>
+      <button>Search</button>
+      <label>
+        Name: 
+        <input />
+      </label>
+    </main>
+    <hr />
+    <footer>
+      <p>&copy; 2023 Ace Coder</p>
+    </footer>
+  </>;
+};
+
+export default App;
