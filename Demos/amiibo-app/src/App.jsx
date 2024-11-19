24f46039ca5fdd4c9d937cc9bd9f1eec40bc80b8
Will Forbes <wforbes1313@gmail.com>
2024-11-19 18:13:52 -0500

    Fixed code from class

--- Demos/amiibo-app/src/App.jsx
+++ Demos/amiibo-app/src/App.jsx
@@ -1,42 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './App.css'
-import { useEffect } from "react";
-import { loadXHR } from "./Ajax";
+import {loadXHR} from './ajax'
+import { readFromLocalStorage, writeToLocalStorage } from "./storage";
 
 // app "globals" and utils
 const baseurl = "https://www.amiiboapi.com/api/amiibo/?name=";
 
 
 
+
+
 const searchAmiibo = (name, callback) => {
-  loadXHR(`${baseurl}${name}`, callback);
+  loadXHR( `${baseurl}${name}`, callback);
 };
 
-// call searchAmiibo() with "mario" and our callback function
+
+
+
+// // call searchAmiibo() with "mario" and our callback function
 // searchAmiibo("mario", parseAmiiboResult); // DONE
 
 const App = () => {
+
   const parseAmiiboResult = xhr => {
+  
     // get the `.responseText` string
-    const responeString = xhr.responseText;
+    const responseString = xhr.responseText;
+  
+    //console.log(responseString);
+   
     // declare a json variable
-    let json = JSON
+    let json = JSON;
+   
     // try to parse the string into a json object
-    json = json.parse(responeString);
-    setResults(json.amiibo)
+    json = json.parse(responseString);
 
-
-    // log out number of results (length of `json.amiibo`)
-    // console.log(`Number of results=${json.amiibo.length}`);
-
-    // // loop through `json.amiibo` and log out the character name
-    // for (let obj of json.amiibo) {
-    //   console.log(obj.name);
-    // }
+    setResults(json.amiibo);
+  
   };
 
-  const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
+  const [term,setTerm] = useState("");
+  const [results,setResults] = useState([]);
 
   useEffect(() => {
     writeToLocalStorage("term", term);
@@ -48,23 +52,23 @@ const App = () => {
     </header>
     <hr />
     <main>
-      <button onClick={() => searchAmiibo(term, parseAmiiboResult)}>Search</button>
+      <button onClick={()=>searchAmiibo(term,parseAmiiboResult) } >Search</button>
       <label>
-        Name:
-        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        Name: 
+        <input value={term} onChange={(e)=>setTerm(e.target.value) } />
       </label>
     </main>
     <hr />
     {results.map(amiibo => (
-      <span key={amiibo.head + amiibo.tail} style={{ color: "green" }}>
-        <h4>{amiibo.name}</h4>
-        <img
-          width="100"
-          alt={amiibo.character}
-          src={amiibo.image}
-        />
-      </span>
-    ))}
+        <span key={amiibo.head + amiibo.tail} style={{color:"green"}}>
+          <h4>{amiibo.name}</h4>
+          <img 
+            width="100" 
+            alt={amiibo.character}
+            src={amiibo.image}
+          />
+        </span>
+      ))}
     <footer>
       <p>&copy; 2023 Ace Coder</p>
     </footer>
--- Demos/amiibo-app/src/main.jsx
+++ Demos/amiibo-app/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+const storeName = "abc1234-amiibo-app";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const loadJSONFromLocalStorage = () => {
+  const string = localStorage.getItem(storeName);
+  let json;
+  try{
+    json = JSON.parse(string);
+    if(!json) throw new Error("json is null!");
+  }catch(error){
+    console.log(`ERROR: ${error} with string: ${string}`);
+    json = {};
+  }
+  return json;
+};
+
+export const writeToLocalStorage = (key, value) => {
+  console.log(`Calling writeToLocalStorage(${key},${value})`);
+  const json = loadJSONFromLocalStorage();
+  json[key] = value;
+  localStorage.setItem(storeName, JSON.stringify(json));
+};
+
+export const readFromLocalStorage = (key) => {
+  const json = loadJSONFromLocalStorage();
+  console.log(`Calling readFromLocalStorage(${key}) with value=${json[key]}`);
+  return json[key];
+}
