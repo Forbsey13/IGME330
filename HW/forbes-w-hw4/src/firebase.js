1b85bdb44df55134b5c55219821f3113cc5e38cb
Will Forbes <wforbes1313@gmail.com>
2024-12-02 14:37:13 -0500

    Fixed configs

--- HW/forbes-w-hw4/src/firebase.js
+++ HW/forbes-w-hw4/src/firebase.js
@@ -1,38 +1,39 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-// import { getDatabase, ref, runTransaction } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getDatabase, ref, runTransaction } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
 
-// let app;
+let app;
 
-// export const initApp = () => {
-//     const firebaseConfig = {
-//         apiKey: "AIzaSyBN5ML1DZiiIlwwZ9p6wF45d0TEyQc0BTI",
-//         authDomain: "high-scores-56600.firebaseapp.com",
-//         projectId: "high-scores-56600",
-//         storageBucket: "high-scores-56600.firebasestorage.app",
-//         messagingSenderId: "307360192328",
-//         appId: "1:307360192328:web:5285f8e5fdcaf12e04857d"
-//     };
+export const initApp = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyDbUBasEGqxCgoca2IzkrjEAuLhjkScBXQ",
+        authDomain: "forbes-w-hw4.firebaseapp.com",
+        projectId: "forbes-w-hw4",
+        storageBucket: "forbes-w-hw4.firebasestorage.app",
+        messagingSenderId: "1079526434067",
+        appId: "1:1079526434067:web:5bb63b33db5e0d6978a6e6",
+        measurementId: "G-3T1WJH1GJE"
+      };
 
-//     app = initializeApp(firebaseConfig);
-// };
+    app = initializeApp(firebaseConfig);
+};
 
-// export const retrieveParkData = () => {
-//     const db = getDatabase(app);
-//     const likesRef = ref(db, 'park');
+export const retrieveParkData = () => {
+    const db = getDatabase(app);
+    const likesRef = ref(db, 'park');
 
-//     return likesRef;
-// };
+    return likesRef;
+};
 
-// export const updateLikeCount = (parkId, increment = true) => {
-//     const db = getDatabase(app);
-//     const likeRef = ref(db, `park/${parkId}`);
+export const updateLikeCount = (parkId, increment = true) => {
+    const db = getDatabase(app);
+    const likeRef = ref(db, `park/${parkId}`);
 
-//     // https://modularfirebase.web.app/reference/database.runtransaction
-//     // runTransaction is a better set so i used it instead of set from your demo
-//     runTransaction(likeRef, (currentLikes) => {
-//         if (currentLikes === null) {
-//             return increment ? 1 : 0;
-//         }
-//         return increment ? currentLikes + 1 : Math.max(0, currentLikes - 1);
-//     });
-// };
+    // https://modularfirebase.web.app/reference/database.runtransaction
+    // runTransaction is a better set so i used it instead of set from your demo
+    runTransaction(likeRef, (currentLikes) => {
+        if (currentLikes === null) {
+            return increment ? 1 : 0;
+        }
+        return increment ? currentLikes + 1 : Math.max(0, currentLikes - 1);
+    });
+};
